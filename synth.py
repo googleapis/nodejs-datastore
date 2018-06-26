9ad8d14eb60eb5abe29f80585d5bc2bc4b23d7a6
Chris Wilcox <crwilcox@google.com>
2018-06-26 10:15:57 -0700

    update gax and add synth.py

---
+++ synth.py
@@ -0,0 +1,29 @@
+import synthtool as s
+import synthtool.gcp as gcp
+import logging
+from pathlib import Path
+import subprocess
+
+logging.basicConfig(level=logging.DEBUG)
+
+gapic = gcp.GAPICGenerator()
+
+# tasks has two product names, and a poorly named artman yaml
+version = 'v1'
+library = gapic.node_library(
+    'datastore', version, 
+    config_path='/google/datastore/artman_datastore.yaml')
+
+# skip index, protos, package.json, and README.md
+s.copy(
+    library,
+    excludes=['package.json', 'README.md', 'src/index.js',
+              f'src/{version}/index.js'])
+#
+# Node.js specific cleanup
+#
+subprocess.run(['npm', 'install'])
+
+# # prettify and lint
+subprocess.run(['npm', 'run', 'prettier'])
+subprocess.run(['npm', 'run', 'lint'])
