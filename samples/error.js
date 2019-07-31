87d30a54134675e748662a44ffa3cfba47736328
vishald123 <44579906+vishald123@users.noreply.github.com>
2019-07-31 12:17:56 -0400

    lint_fix

--- samples/error.js
+++ samples/error.js
@@ -11,54 +11,54 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * @see [Cloud Datastore Concepts errors] {@link https://cloud.google.com/datastore/docs/concepts/errors}
  * for more information on errors
- */	
+ */
 
-'use strict';	
+'use strict';
 
-const {Datastore} = require('@google-cloud/datastore');	
+const {Datastore} = require('@google-cloud/datastore');
 
-// [START error]	
-function runQuery() {	
-  // Creates a client	
-  const datastore = new Datastore();	
+// [START error]
+function runQuery() {
+  // Creates a client
+  const datastore = new Datastore();
 
-  const query = datastore.createQuery(['Company']).start('badrequest');	
+  const query = datastore.createQuery(['Company']).start('badrequest');
 
-  return datastore	
-    .runQuery(query)	
-    .then(results => {	
-      const entities = results[0];	
-      console.log('Entities:');	
-      entities.forEach(entity => console.log(entity));	
-      return entities;	
-    })	
+  return datastore
+    .runQuery(query)
+    .then(results => {
+      const entities = results[0];
+      console.log('Entities:');
+      entities.forEach(entity => console.log(entity));
+      return entities;
+    })
     .catch(err => {
       console.log(err.code); //400
       console.log(err.message); //"Key path is incomplete: [Person: null]"
       console.log(err.status); //"INVALID_ARGUMENT"
       /**
-      *  @see [For more information on error codes refer] https://cloud.google.com/datastore/docs/concepts/errors#error_codes
-      */ 
+       *  @see [For more information on error codes refer] https://cloud.google.com/datastore/docs/concepts/errors#error_codes
+       */
 
       // Process error
 
-      // For example, treat permission error like no entities were found	
+      // For example, treat permission error like no entities were found
       // eslint-disable-next-line no-constant-condition
-      if (/* some condition */ false) {	
-        return [];	
-      }  
-         
+      if (/* some condition */ false) {
+        return [];
+      }
+
       //Forward the error to caller
       return Promise.reject(err);
-    });	
-}	
-// [END error]	
+    });
+}
+// [END error]
 
-exports.runQuery = runQuery;	
+exports.runQuery = runQuery;
 
-if (module === require.main) {	
-  exports.runQuery();	
-}	
+if (module === require.main) {
+  exports.runQuery();
+}
