8148a1b20bf237207d3166af346a349c197584be
vishald123 <44579906+vishald123@users.noreply.github.com>
2019-08-20 09:48:02 -0400

    test

--- samples/error.js
+++ samples/error.js
@@ -1,19 +1,17 @@
 /**
- * Copyright 2018, Google LLC.
+ * Copyright 2019 Google LLC
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *   https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- * @see [Cloud Datastore Concepts errors] {@link https://cloud.google.com/datastore/docs/concepts/errors}
- * for more information on errors
  */
 
 'use strict';
@@ -23,7 +21,7 @@
 //  description: sample show how to handle and process error
 //  usage: node error.js
 
-const {Datastore} = require('@google-cloud/datastore');
+const { Datastore } = require('@google-cloud/datastore');
 
 // [START error]
 function main() {
