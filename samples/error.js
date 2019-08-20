5dd8d0dbbd4c8ca77bdc6c5321be7aceed04957e
Alex <avaksman@gmail.com>
2019-08-20 10:13:10 -0400

    refactor: header

--- samples/error.js
+++ samples/error.js
@@ -1,5 +1,6 @@
 /**
- * Copyright 2018, Google, Inc.
+ * Copyright 2019 Google LLC.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -11,9 +12,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- * @see [Cloud Datastore Concepts errors] {@link https://cloud.google.com/datastore/docs/concepts/errors}
- * for more information on errors
  */
 
 'use strict';
