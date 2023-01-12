6c23ccb398d08ed42dac871b284d8f6a75e10447
Daniel Bruce <djbruce@google.com>
2023-01-12 15:10:32 -0500

    Commented functions and classes

--- src/aggregate.ts
+++ src/aggregate.ts
@@ -17,26 +17,57 @@ import {RunQueryOptions, RunQueryResponse} from './query';
 import {RequestCallback} from './request';
 const AGGREGATE_QUERY = Symbol('AGGREGATE_QUERY');
 
+/**
+ * An AggregateQuery is a class that can be used to obtain results from an
+ * aggregate query request.
+ *
+ * @see {@link https://cloud.google.com/datastore/docs/aggregation-queries| Aggregation queries Reference}
+ *
+ * @class
+ */
 class AggregateQuery {
   type = AGGREGATE_QUERY;
   aggregations: Array<AggregateField>;
   query: Query | undefined;
 
+  /**
+   * Build an AggregateQuery object.
+   *
+   * @param {Query} query
+   */
   constructor(query: Query) {
     this.query = query;
     this.aggregations = [];
   }
 
+  /**
+   * Add a `count` aggregate query to the list of aggregations.
+   *
+   * @param {string} alias
+   * @returns {AggregateQuery}
+   */
   count(alias: string): AggregateQuery {
     this.aggregations.push(AggregateField.count().alias(alias));
     return this;
   }
 
+  /**
+   * Add a custom aggregation to the list of aggregations.
+   *
+   * @param {AggregateField} aggregation
+   * @returns {AggregateQuery}
+   */
   addAggregation(aggregation: AggregateField): AggregateQuery {
     this.aggregations.push(aggregation);
     return this;
   }
 
+  /**
+   * Add a list of custom aggregations to the list of aggregations.
+   *
+   * @param {AggregateField[]} aggregation
+   * @returns {AggregateQuery}
+   */
   addAggregations(aggregations: AggregateField[]): AggregateQuery {
     for (const aggregation of aggregations) {
       this.aggregations.push(aggregation);
@@ -44,6 +75,13 @@ class AggregateQuery {
     return this;
   }
 
+  /**
+   * Run the aggregation query and return the results.
+   *
+   * @param {RunQueryOptions | RequestCallback} [optionsOrCallback]
+   * @param {function} cb The callback function.
+   * @returns {void | Promise<RunQueryResponse>}
+   */
   run(
     optionsOrCallback?: RunQueryOptions | RequestCallback,
     cb?: RequestCallback
@@ -57,30 +95,62 @@ class AggregateQuery {
     return runAggregationQuery(this, options, callback);
   }
 
+  /**
+   * Get the proto for the list of aggregations.
+   *
+   */
   // eslint-disable-next-line
   toProto(): any {
     return this.aggregations.map(aggregation => aggregation.toProto());
   }
 }
 
+/**
+ * An AggregateField is a class that contains data that defines an aggregation.
+ *
+ */
 abstract class AggregateField {
   alias_?: string;
 
+  /**
+   * Gets a copy of the Count aggregate field.
+   *
+   * @returns {Count}
+   */
   static count(): Count {
     return new Count();
   }
 
+  /**
+   * Gets a copy of the Count aggregate field.
+   *
+   * @param {string} alias The label used in the results to describe this
+   * aggregate field when a query is run.
+   * @returns {AggregateField}
+   */
   alias(alias: string): AggregateField {
     this.alias_ = alias;
     return this;
   }
 
+  /**
+   * Gets the proto for the aggregate field.
+   *
+   */
   // eslint-disable-next-line
   abstract toProto(): any;
 }
 
+/**
+ * A Count is a class that contains data that defines a Count aggregation.
+ *
+ */
 class Count extends AggregateField {
   // eslint-disable-next-line
+  /**
+   * Gets the proto for the count aggregate field.
+   *
+   */
   toProto(): any {
     const count = Object.assign({});
     return Object.assign({count}, this.alias_ ? {alias: this.alias_} : null);
