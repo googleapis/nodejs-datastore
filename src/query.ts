6779faf1806b9c2b246ac11a3883c7ba89298739
Daniel Bruce <djbruce@google.com>
2023-03-01 10:12:51 -0500

    Revert "Revert "Now removes deprecation warning properly""
    This reverts commit 8e96bf5e5ef11593853ae061fd1858c2e463d6ce.

--- src/query.ts
+++ src/query.ts
@@ -218,6 +218,9 @@ class Query {
       this.newFilters.push(propertyOrFilter);
       return this;
     } else {
+      process.emitWarning(
+        'Providing Filter objects is recommended when using .filter'
+      );
       let operator = operatorOrValue as Operator;
       if (arguments.length === 2) {
         value = operatorOrValue as {};
