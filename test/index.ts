c9856b2880c70db169d4362a466ba66f8688f04c
Daniel Bruce <djbruce@google.com>
2023-03-21 11:47:12 -0400

    correction to test against old database
    This was a typo. We want to test against the database we were using before

--- test/index.ts
+++ test/index.ts
@@ -89,7 +89,7 @@ function fakeGoogleAuth(...args: Array<{}>) {
 
 let createInsecureOverride: Function | null;
 
-const SECOND_DATABASE_ID = 'foo';
+const SECOND_DATABASE_ID = 'foo2';
 
 export {SECOND_DATABASE_ID};
 
