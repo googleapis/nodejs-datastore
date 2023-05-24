f139770f865f82e20022ce8c4c4c4b98a899f8bb
Daniel Bruce <djbruce@google.com>
2023-05-24 16:50:26 -0400

    Do a refactor on the test
    Objects shared by both tests should be moved into the outer block so that they can be shared.

--- test/no-mocks.ts
+++ test/no-mocks.ts
@@ -4,16 +4,15 @@ import * as assert from 'assert';
 import {Datastore} from '../src';
 
 describe('NoMocks', () => {
-  const datastore = new Datastore({
-    namespace: `${Date.now()}`,
-  });
-
-  it.only('should encode a request without excludeFromIndexes', done => {
+  describe('using save to evaluate excludeFromIndexes', () => {
+    const datastore = new Datastore({
+      namespace: `${Date.now()}`,
+    });
     const namespace = datastore.namespace;
     const expectedConfig = {
       client: 'DatastoreClient',
       method: 'commit',
-      // gaxOpts: {},
+      gaxOpts: {},
       reqOpts: {
         mutations: [
           {
@@ -30,35 +29,40 @@ describe('NoMocks', () => {
         ],
       },
     };
-    datastore.request_ = (config: RequestConfig) => {
+    function evaluateConfig(config: RequestConfig) {
       try {
         assert.deepStrictEqual(config, expectedConfig);
       } catch (e) {
         console.log(e);
         assert.fail('assertion failed');
       }
-      done();
-    };
-    const key = datastore.key(['Post', 'Post1']);
-    datastore.save({
-      key,
-      data: {},
-    });
-  });
+    }
 
-  it('should ignore non-existent property in excludeFromIndexes', done => {
-    datastore.request_ = (config: RequestConfig) => {
-      console.log(config);
-      done();
-    };
-    const key = datastore.key(['Post', 'Post1']);
-    datastore.save({
-      key,
-      data: {},
-      excludeFromIndexes: [
-        'non_exist_property', // this just ignored
-        'non_exist_property.*', // should also be ignored
-      ],
+    it.only('should encode a request without excludeFromIndexes', done => {
+      datastore.request_ = (config: RequestConfig) => {
+        evaluateConfig(config);
+        done();
+      };
+      const key = datastore.key(['Post', 'Post1']);
+      datastore.save({
+        key,
+        data: {},
+      });
+    });
+    it('should ignore non-existent property in excludeFromIndexes', done => {
+      datastore.request_ = (config: RequestConfig) => {
+        evaluateConfig(config);
+        done();
+      };
+      const key = datastore.key(['Post', 'Post1']);
+      datastore.save({
+        key,
+        data: {},
+        excludeFromIndexes: [
+          'non_exist_property', // this just ignored
+          'non_exist_property.*', // should also be ignored
+        ],
+      });
     });
   });
 });
