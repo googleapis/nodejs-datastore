b798ec3da29a9f43f654f843711713b19b8c63d3
Daniel Bruce <djbruce@google.com>
2023-09-28 16:03:39 -0400

    Revert "Define isFirstPathPartUndefined"
    This reverts commit df3f3769926f62bf1ed3ea92bb5aef85974a1e42.

--- src/entity.ts
+++ src/entity.ts
@@ -843,8 +843,6 @@ export namespace entity {
       const splitPath = path.split(delimiter);
       const firstPathPart = splitPath.shift()!;
       const remainderPath = splitPath.join(delimiter).replace(/^(\.|\[\])/, '');
-      const isFirstPathPartUndefined =
-        entity.properties![firstPathPart] !== undefined;
 
       if (
         !(entity.properties && entity.properties[firstPathPart]) &&
@@ -857,7 +855,7 @@ export namespace entity {
       if (
         firstPathPartIsArray &&
         // check also if the property in question is actually an array value.
-        isFirstPathPartUndefined &&
+        entity.properties![firstPathPart] !== undefined &&
         entity.properties![firstPathPart].arrayValue &&
         // check if wildcard is not applied
         !hasWildCard
@@ -886,7 +884,7 @@ export namespace entity {
         firstPathPartIsArray &&
         hasWildCard &&
         remainderPath === '*' &&
-        isFirstPathPartUndefined
+        entity.properties![firstPathPart] !== undefined
       ) {
         const array = entity.properties![firstPathPart].arrayValue;
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -909,7 +907,7 @@ export namespace entity {
           if (
             hasWildCard &&
             remainderPath === '*' &&
-            isFirstPathPartUndefined
+            entity.properties![firstPathPart] !== undefined
           ) {
             const parentEntity = entity.properties![firstPathPart].entityValue;
 
@@ -923,7 +921,7 @@ export namespace entity {
             } else {
               excludePathFromEntity(entity, firstPathPart);
             }
-          } else if (isFirstPathPartUndefined) {
+          } else if (entity.properties![firstPathPart] !== undefined) {
             const parentEntity = entity.properties![firstPathPart].entityValue;
             excludePathFromEntity(parentEntity, remainderPath);
           }
