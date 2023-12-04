d066264de4d6182e0cfa6dde1487a71bca8db97a
Daniel Bruce <djbruce@google.com>
2023-12-04 14:12:11 -0500

    Regroup the code so it is better arranged
    Proper describe blocks make it easier for the code to be arranged so that with and without rest tests can be incorporated into the code.

--- test/client-testing.ts
+++ test/client-testing.ts
@@ -1,31 +1,15 @@
 import {before, beforeEach, describe, it} from 'mocha';
-import {Datastore, DatastoreClient, Fallback} from '../src';
+import {DatastoreClient, Fallback} from '../src';
 import * as assert from 'assert';
 import * as proxyquire from 'proxyquire';
-import {Callback, CallOptions, ClientStub} from 'google-gax';
+import {Callback, CallOptions} from 'google-gax';
 import * as protos from '../protos/protos';
 import * as ds from '../src';
-import {RequestConfig} from '../src/request';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 type Any = any;
 
 class FakeDatastoreClient extends DatastoreClient {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  constructor(...args: any[]) {
-    assert.strictEqual(args[0].fallback, 'rest');
-    super();
-  }
-
-  static get getString() {
-    return 'some-string';
-  }
-  static get scopes() {
-    return [
-      'https://www.googleapis.com/auth/cloud-platform',
-      'https://www.googleapis.com/auth/datastore',
-    ];
-  }
   lookup(
     request?: protos.google.datastore.v1.ILookupRequest,
     optionsOrCallback?:
@@ -50,8 +34,23 @@ class FakeDatastoreClient extends DatastoreClient {
     if (callback) {
       callback(new Error('some error'));
     }
-    console.log('getting');
     return new Promise(() => {});
+  }
+}
+
+class FakeDatastoreClientExpectingRest extends FakeDatastoreClient {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  constructor(...args: any[]) {
+    assert.strictEqual(args[0].fallback, 'rest');
+    super();
+  }
+}
+
+class FakeDatastoreClientExpectingGrpc extends FakeDatastoreClient {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  constructor(...args: any[]) {
+    assert.strictEqual(args[0].fallback, 'rest');
+    super();
   }
 }
 
@@ -60,45 +59,37 @@ describe('ClientTesting', () => {
   let Request: typeof ds.DatastoreRequest;
   let request: Any;
 
-  before(() => {
-    Request = proxyquire('../src/request', {
-      './v1': {
-        DatastoreClient: FakeDatastoreClient,
-      },
-    }).DatastoreRequest;
-    Datastore = proxyquire('../src', {
-      './request': {
-        Request,
-      },
-      './v1': {
-        DatastoreClient: FakeDatastoreClient,
-      },
-    }).Datastore;
-  });
-
-  beforeEach(() => {
-    request = new Request();
-  });
-
   describe('rest parameter support', () => {
     const clientName = 'DatastoreClient';
-    // it.only('should not set the rest parameter in the data client when it is not provided', async () => {});
-    it.only('should set the rest parameter in the data client when it is provided', done => {
-      // TODO: Current state, client is not mocking out the v1
-      const options = {
-        fallback: 'rest' as Fallback,
-      };
-      const otherDatastore = new Datastore(options);
-      // @ts-ignore
-      // const datastoreClient = new FakeDatastoreClient() as ClientStub;
-      // otherDatastore.clients_.set(clientName, datastoreClient);
-      request.datastore = otherDatastore;
-      request.prepareGaxRequest_(
-        {client: clientName, method: 'lookup'},
-        (err: any, res: any) => {
-          done();
-        }
-      );
+    describe('when the datastore client is expecting a rest parameter', () => {
+      before(() => {
+        Request = proxyquire('../src/request', {
+          './v1': {
+            DatastoreClient: FakeDatastoreClient,
+          },
+        }).DatastoreRequest;
+        Datastore = proxyquire('../src', {
+          './request': {
+            Request,
+          },
+          './v1': {
+            DatastoreClient: FakeDatastoreClient,
+          },
+        }).Datastore;
+        request = new Request();
+      });
+      it.only('should set the rest parameter in the data client when calling prepareGaxRequest_', done => {
+        const options = {
+          fallback: 'rest' as Fallback,
+        };
+        request.datastore = new Datastore(options);
+        request.prepareGaxRequest_(
+          {client: clientName, method: 'lookup'},
+          (err: any, res: any) => {
+            done();
+          }
+        );
+      });
     });
   });
 });
