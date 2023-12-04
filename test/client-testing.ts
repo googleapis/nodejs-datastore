4b8ab09bc72903ea8de6fbc2bc81dfaff32cbef9
Daniel Bruce <djbruce@google.com>
2023-12-04 10:46:15 -0500

    This harness works properly
    The harness mocks out the constructor and calls reach the constructor as well as the get function. So now we can see what values get passed into the constructor and get function.

--- test/client-testing.ts
+++ test/client-testing.ts
@@ -1,58 +1,52 @@
-import {before, describe, it} from 'mocha';
+import {describe, it} from 'mocha';
 import {Datastore, DatastoreClient, Fallback} from '../src';
 import * as assert from 'assert';
-import * as proxyquire from 'proxyquire';
-import {Callback, CallOptions} from 'google-gax';
+import {Callback, CallOptions, ClientStub} from 'google-gax';
 import * as protos from '../protos/protos';
-import * as ds from '../src';
 
-let v1FakeClientOverride: Function | null;
-const fakeV1 = {
-  DatastoreClient: class {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    constructor(...args: any[]) {
-      console.log('constructing');
-      return (v1FakeClientOverride || (() => {}))(...args);
-    }
+class FakeDatastoreClient extends DatastoreClient {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  constructor(...args: any[]) {
+    console.log('constructing');
+    super();
+  }
 
-    static get scopes() {
-      return [
-        'https://www.googleapis.com/auth/cloud-platform',
-        'https://www.googleapis.com/auth/datastore',
-      ];
+  static get scopes() {
+    return [
+      'https://www.googleapis.com/auth/cloud-platform',
+      'https://www.googleapis.com/auth/datastore',
+    ];
+  }
+  lookup(
+    request?: protos.google.datastore.v1.ILookupRequest,
+    optionsOrCallback?:
+      | CallOptions
+      | Callback<
+          protos.google.datastore.v1.ILookupResponse,
+          protos.google.datastore.v1.ILookupRequest | null | undefined,
+          {} | null | undefined
+        >,
+    callback?: Callback<
+      protos.google.datastore.v1.ILookupResponse,
+      protos.google.datastore.v1.ILookupRequest | null | undefined,
+      {} | null | undefined
+    >
+  ): Promise<
+    [
+      protos.google.datastore.v1.ILookupResponse,
+      protos.google.datastore.v1.ILookupRequest | undefined,
+      {} | undefined,
+    ]
+  > {
+    if (callback) {
+      callback(new Error('some error'));
     }
-    lookup(
-      request?: protos.google.datastore.v1.ILookupRequest,
-      optionsOrCallback?:
-        | CallOptions
-        | Callback<
-            protos.google.datastore.v1.ILookupResponse,
-            protos.google.datastore.v1.ILookupRequest | null | undefined,
-            {} | null | undefined
-          >,
-      callback?: Callback<
-        protos.google.datastore.v1.ILookupResponse,
-        protos.google.datastore.v1.ILookupRequest | null | undefined,
-        {} | null | undefined
-      >
-    ) {
-      if (callback) {
-        callback(new Error('some error'));
-      }
-      console.log('getting');
-    }
-  },
-};
+    console.log('getting');
+    return new Promise(() => {});
+  }
+}
 
 describe('ClientTesting', () => {
-  let Datastore: typeof ds.Datastore;
-
-  before(() => {
-    Datastore = proxyquire('../src', {
-      './v1': fakeV1,
-    }).Datastore;
-  });
-
   describe('rest parameter support', () => {
     const clientName = 'DatastoreClient';
     // it.only('should not set the rest parameter in the data client when it is not provided', async () => {});
@@ -63,11 +57,13 @@ describe('ClientTesting', () => {
         fallback: 'rest' as Fallback,
       };
       const otherDatastore = new Datastore(options);
+      // @ts-ignore
+      const datastoreClient = new FakeDatastoreClient() as ClientStub;
+      otherDatastore.clients_.set(clientName, datastoreClient);
       const keys = otherDatastore.key(['Company', 'Google']);
       await otherDatastore.get(keys);
       const dataClient = otherDatastore.clients_.get(clientName);
       assert(dataClient);
-      const projectId = await otherDatastore.getProjectId();
     });
   });
 });
