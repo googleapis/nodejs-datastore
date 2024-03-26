8de1f84aaa89490d53c2f368c19a6ce16d1b5c0d
Daniel Bruce <djbruce@google.com>
2024-03-26 14:53:58 -0400

    Remove calls to nightly
    Feature is now fully ready so nightly tests should not be done because the feature is expected to work in production.

--- system-test/datastore.ts
+++ system-test/datastore.ts
@@ -34,12 +34,10 @@ async.each(
   [
     {
       namespace: `${Date.now()}`,
-      apiEndpoint: 'nightly-datastore.sandbox.googleapis.com', // TODO: Remove before merge
     },
     {
       databaseId: SECOND_DATABASE_ID,
       namespace: `second-db-${Date.now()}`,
-      apiEndpoint: 'nightly-datastore.sandbox.googleapis.com', // TODO: Remove before merge
     },
   ],
   (clientOptions: DatastoreOptions) => {
