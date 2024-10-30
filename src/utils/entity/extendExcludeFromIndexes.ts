9bef8e4acb6873ecc7150210031a72a04c9b6275
Daniel Bruce <djbruce@google.com>
2024-10-30 17:30:56 -0400

    Refactor the save function
    Pull out the code that extends the excludeFromIndexes list

---
+++ src/utils/entity/extendExcludeFromIndexes.ts
@@ -0,0 +1,37 @@
+import {entity, Entity} from '../../entity';
+
+/**
+ * This function extends the excludeFromIndexes list when it finds
+ * large properties in the entity object. The extended excludeFromIndexes
+ * list is then used when building the entity proto.
+ */
+export function extendExcludeFromIndexes(entityObject: Entity) {
+  if (entityObject.excludeLargeProperties) {
+    if (Array.isArray(entityObject.data)) {
+      // This code populates the excludeFromIndexes list with the right values.
+      if (entityObject.excludeLargeProperties) {
+        entityObject.data.forEach(
+          (data: {
+            name: {
+              toString(): string;
+            };
+            value: Entity;
+            excludeFromIndexes?: boolean;
+          }) => {
+            entityObject.excludeFromIndexes = entity.findLargeProperties_(
+              data.value,
+              data.name.toString(),
+              entityObject.excludeFromIndexes
+            );
+          }
+        );
+      }
+    } else {
+      entityObject.excludeFromIndexes = entity.findLargeProperties_(
+        entityObject.data,
+        '',
+        entityObject.excludeFromIndexes
+      );
+    }
+  }
+}
